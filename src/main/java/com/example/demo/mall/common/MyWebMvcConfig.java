b9bf3ff3ff800caf7b1a970f940ef445b098e651
xchnba <827670131@qq.com>
2019-11-25 23:17:16 +0800

    获取静态资源地址

---
+++ src/main/java/com/example/demo/mall/common/MyWebMvcConfig.java
@@ -0,0 +1,14 @@
+//package com.example.demo.mall.common;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class MyWebMvcConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        // 将/static/**访问映射到classpath:/mystatic/
+//        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/mystatic/");
+//    }
+//}
---
+++ src/main/java/com/example/demo/mall/common/WebMvcConfig.java
@@ -0,0 +1,25 @@
+//package com.example.demo.mall.common;
+//
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * 配置静态资源映射
+// *
+// * @author sunziwen
+// * @version 1.0
+// * @date 2018-11-16 14:57
+// **/
+//@Component
+//public class WebMvcConfig implements WebMvcConfigurer {
+//    /**
+//     * 添加静态资源文件，外部可以直接访问地址
+//     *
+//     * @param registry
+//     */
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+//    }
+//}
